e9ae22e9e64c751f81dadc75f78b0db0a3b5a7f7
berlink2 <berlink.k2@gmail.com>
2020-09-27 22:05:58 +0700

    Updates

--- precache-manifest.b56326410adce831a92b0b252171285b.js
+++ precache-manifest.b56326410adce831a92b0b252171285b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a8f639f0612e48ea52bfb0c899621d5",
+    "revision": "f08a684cf6e89d9430f02d2306164a5a",
     "url": "/finantier-test-fe/index.html"
   },
   {
-    "revision": "de1d80fa44f6b19ac5d1",
+    "revision": "7923c6f4ddd8018b4355",
     "url": "/finantier-test-fe/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "5100a82185b1adf31c59",
+    "revision": "21f33a49ee9625498d89",
     "url": "/finantier-test-fe/static/css/main.d56ec273.chunk.css"
   },
   {
-    "revision": "de1d80fa44f6b19ac5d1",
-    "url": "/finantier-test-fe/static/js/2.9d80e97a.chunk.js"
+    "revision": "7923c6f4ddd8018b4355",
+    "url": "/finantier-test-fe/static/js/2.68771a20.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/finantier-test-fe/static/js/2.9d80e97a.chunk.js.LICENSE.txt"
+    "url": "/finantier-test-fe/static/js/2.68771a20.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5100a82185b1adf31c59",
-    "url": "/finantier-test-fe/static/js/main.f997abb2.chunk.js"
+    "revision": "21f33a49ee9625498d89",
+    "url": "/finantier-test-fe/static/js/main.b8140576.chunk.js"
   },
   {
     "revision": "d007e5a4a915b577bfde",
